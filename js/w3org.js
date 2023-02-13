3016cbbcb0f55bb89486e5182adf412138d3702b
DannTerner <125185667+DannTerner@users.noreply.github.com>
2023-02-13 18:54:44 +0200

    Update w3org.js

--- js/w3org.js
+++ js/w3org.js
@@ -1,9 +1,16 @@
-$('document').ready(function(){
+$('document').ready(function() {
     loadGoods();
 });
+
 function loadGoods() {
     // Загружаю товари на страницу
     $.getJSon('goods.json', function(date) {
-        console.log(data);
+        // console.log(data);
+        var uot = '';
+        for (var key in data) {
+            out += '<p>' += dsate[key]['name'] + '</p>';
+            out += '<p>' += dsate[key]['cost'] + '</p>';
+        }
+        $('goods').html(uot);
     })
-} 
+}
